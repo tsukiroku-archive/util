a59db7b06f8d29c9306cad94ffc2ef46fffcea1b
tsukiroku <tsukiroku@yahoo.com>
2022-05-05 21:20:36 +0900

    Update match.ts

--- util/match.ts
+++ util/match.ts
@@ -3,6 +3,23 @@ interface IMatch<T> {
     then: (v?: T) => any;
 }
 
+/**
+ * 
+ * @param v Generic T
+ * @param matchs Array of IMatch<T>
+ *
+ * # Example:
+ * ```ts
+ * match('3',
+ *      { v: '1', then: () => "one" },
+ *      { v: '2', then: () => "two" },
+ *      { v: '3', then: () => "three" },
+ *      { then: (v?: string) => `other: ${v}` },
+ * );
+ * ```
+ * # Result:
+ * `three`
+ */
 const match = <T>(v: T, ...matchs: Array<IMatch<T>>) => {
     let result: T = v;
     if (!matchs.includes({ v: v, then: () => { } })) {
