9799b29c9ebc403f4a9b764f73b378027f3e6469
tsukiroku <tsukiroku@yahoo.com>
2022-05-08 15:53:20 +0900

    Update test.js

--- dist/test.js
+++ dist/test.js
@@ -4,4 +4,4 @@ const util_1 = require("./util");
 console.log((0, util_1.arrayToObject)(["hello", "world"], (k) => {
     return `${k}!`;
 }));
-console.log((0, util_1.match)("3", { v: "1", then: () => "one" }, { v: "2", then: () => "two" }, { v: "3", then: () => "three" }, { then: (v) => `other: ${v}` })); // three
+console.log((0, util_1.match)("4", { v: "1", then: () => 1 }, { v: "2", then: () => 2 }, { v: "3", then: () => 3 }, { then: (v) => `other: ${v}` })); // three
